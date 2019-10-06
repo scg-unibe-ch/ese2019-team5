50f589b69ccf4ebde529238d3571283f4d9db443
Sophie Pfister <sophie.pfister@students.unibe.ch>
2019-10-06 14:54:43 +0200

    Added comments
    To-Do:
    1) How to save an account? (Communication with backend)
    2) Styling

--- IonicAndAngularTry/src/app/start/login/login.page.ts
+++ IonicAndAngularTry/src/app/start/login/login.page.ts
@@ -5,6 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+/**
+ * LoginPage
+ * Asks an (existing) user for his authentification data (e-mail, password)
+ * Does entire Authentification-stuff...
+ * To be implemented
+ */
 export class LoginPage implements OnInit {
 
   constructor() { }
