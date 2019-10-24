9ac51e55a79c390a308021360f853e27fb75f632
Sophie Pfister <sophie.pfister@students.unibe.ch>
2019-10-24 14:36:00 +0200

    Added ToDo's for the next iteration

--- frontend/src/app/start/start.page.ts
+++ frontend/src/app/start/start.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../AuthService/auth.service";
 
+//ToDo evtl: HttpService, der Abfragen an Backend stellt
+
+//ToDo: Logged in as... (Im Header) mit Link zu Profilseite
+//ToDo: Profil-Seite (Lars) mit Link zu Service erstellen
+
+//ToDo Visu: div. AngebotPage
+// Zeigt alle in der DB gespeicherten Angebote an
+
 @Component({
   selector: 'app-start',
   templateUrl: './start.page.html',
