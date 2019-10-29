dd3c05bfdf16715a69f81fd0cd07cd80d736076b
Gillian Cathomas <gillian.cathomas@gmx.ch>
2019-10-29 03:15:36 -0700

    added part of biweekly report

--- backend/app/controllers/profile.controller.ts
+++ backend/app/controllers/profile.controller.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 
 const router: Router = Router(); // part of express needed
 
-router.post('/update', async (req: Request, res: Response) => {
+router.put('/update', async (req: Request, res: Response) => {
   try {
     //db.Service.UpdateUser(user); //TODO hier user oder nur das was sich ändert und was muss genau überprüft werden....
 
@@ -12,7 +12,7 @@ router.post('/update', async (req: Request, res: Response) => {
   }
 });
 
-router.post ( '/changepassword', async (req: Request, res: Response)=>{
+router.put( '/changepassword', async (req: Request, res: Response)=>{
   try {
 
   }
