af497b52de6f288543a8b86f59be4bca6bf35994
Gillian Cathomas <gillian.cathomas@gmx.ch>
2019-11-02 06:06:35 -0700

    working version of Builder but not compatible with rest of code yet

--- backend/app/models/user.model.ts
+++ backend/app/models/user.model.ts
@@ -3,74 +3,154 @@ import {Address} from "./address.model";
 import assert from "assert";
 import {UserBuilder} from "./userBuilder.model";
 //TODO unsicher mit addresse, isFirm und phonenumber.... Wie soll das mit dem FOto gehen.....
-export class User {
-  id = -1;
-  firstname: string;
-  lastname: string;
-  email: string;
-  pwhash: string;
-  address: Address;
-  isVerified: boolean;
-  newUser = false;
-  isAdmin = false;
-  isFirm = false; //TODO wie anders lösen?? mit Name oder sonst wie?
-  phoneNumber = "null";
+ export class User {
+   private id = -1;
+   private _firstname: string;
+   private _lastname: string;
+   private _email: string;
+   private _pwhash: string;
+   private _address: Address;
+   private _isVerified: boolean;
+   private _newUser = false;
+   private _isAdmin = false;
+   private _isFirm = false; //TODO wie anders lösen?? mit Name oder sonst wie?
+   private phoneNumber = "null";
+
+
+     constructor(firstname: string, lastname: string, email: string, pwhash: string, isVerified: boolean, address: Address, isFirm: boolean ){//phoneNumber:string) {
+       this._firstname = firstname;
+       this._lastname = lastname;
+       this._email= email;
+    this._pwhash=pwhash;
+    this._address=address;
+    this._isVerified=isVerified;
+    this._isFirm=isFirm;
+    //this.phoneNumber= phoneNumber;
+    }
+
+   public setEmail (email:string){
+     this._email= email;
+     return this;
+   }
+
+   public getEmail(): string {
+     return this._email;
+
+   }
+   public setPwhash (pwhash:string){
+     this._pwhash= pwhash;
+     return this;
+   }
+   public getPwHash(): string {
+     return this._pwhash;
+
+   }
+
+   public setIsFirm (isFirm:boolean){
+     this._isFirm= isFirm;
+     return this;
+   }
+
+   public getIsFirm(): boolean {
+     return this._isFirm;
+
+   }
+   public setAddress (address:Address){
+     this._address= address;
+     return this;
+   }
+   public getAddress(): Address {
+     return this._address;
+
+   }
+
+ }
 
 
 
- constructor(firstname: string, lastname: string, email: string, pwhash: string, isVerified: boolean, address: Address, isFirm: boolean ){//phoneNumber:string) {
-  this.firstname= firstname;
-  this.lastname= lastname;
-  this.email= email;
-  this.pwhash=pwhash;
-  this.address=address;
-  this.isVerified=isVerified;
-  this.isFirm=isFirm;
-  //this.phoneNumber= phoneNumber;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+   constructor(userBuilder: UserBuilder) {
+     this.firstname = userBuilder.firstname;
+     this.lastname = userBuilder.lastname;
+     this.email = userBuilder.email;
+     this.pwhash = userBuilder.pwhash;
+     this.address = userBuilder.address;
+     this.isFirm = userBuilder.isFirm;
+     this.isVerified = userBuilder.isVerified;
+   }*/
+
+/*   get firstname(){
+     return this.firstname;
+   }*/
+
+
+
+
+  /**
+   * sets the id
+   * @param id
+   *!/
+  setId(id:number)
+  {
+    this.id = id;
   }
 
-/* constructor (userBuilder: UserBuilder) {
-  this.firstname = userBuilder.firstname;
-   this.lastname = userBuilder.lastname;
-  this.email = userBuilder.email;
-   this.pwhash = userBuilder.pwhash;
-   this.address = userBuilder.address;
-   this.isFirm = userBuilder.isFirm;
-   this.isVerified = userBuilder.isVerified;
- }*/
+  setPhoneNumber(phoneNumber: string)
+  {
+    this.phoneNumber = phoneNumber;
+  }
+
+  /!**
+   * returns the user element as string //TODO hier addresse auch hin??
+   *!/
+  toString(): string
+  {
+    return String(this.id) + ' ' + this.firstname + ' ' + this.lastname + ' ' + this.email + ' ' + this.pwhash;
+  }
+
+  /!**
+   * returns the firstname and the lastname as string
+   *!/
+  toNameString():string
+  {
+    return this.firstname + ' ' + this.lastname;
+  }
 
 
+  }
+  */
 
-
-/**
- * sets the id
- * @param id
- */
-setId(id:number)
-{
-  this.id = id;
-}
-
-setPhoneNumber(phoneNumber: string)
-{
-  this.phoneNumber = phoneNumber;
-}
-
-/**
- * returns the user element as string //TODO hier addresse auch hin??
- */
-toString(): string
-{
-  return String(this.id) + ' ' + this.firstname + ' ' + this.lastname + ' ' + this.email + ' ' + this.pwhash;
-}
-
-/**
- * returns the firstname and the lastname as string
- */
-toNameString():string
-{
-  return this.firstname + ' ' + this.lastname;
-}
-
-
-}
--- backend/app/models/userBuilder.model.ts
+++ backend/app/models/userBuilder.model.ts
@@ -6,47 +6,147 @@ const mockAddress:Address=new Address('',0,1234,'')
 
 
 export class UserBuilder {
-  private _firstname: string= '';
-  private _lastname: string= '';
-  private _email: string= '';
-  private _pwhash: string= '';
-  private _address: Address=mockAddress;
-  private _isVerified: boolean=false;
-  private _isFrim: boolean=false;
-  private _phoneNumber: string='0000000000';
 
-  constructor(firstname:string,lastname:string){
+  private id = -1;
+  private _firstname: string;
+  private _lastname: string;
+  private _email: string;
+  private _pwhash: string;
+  private _address: Address;
+  private _isVerified: boolean;
+  private _newUser = false;
+  private _isAdmin = false;
+  private _isFirm = false; //TODO wie anders lösen?? mit Name oder sonst wie?
+  private phoneNumber = "null";
+
+  /*private _firstname: string= '';
+    private _lastname: string= '';
+    private _email: string= '';
+    private _pwhash: string= '';
+    private _address: Address=mockAddress;
+    private _isVerified: boolean=false;
+    private _isFrim: boolean=false;
+    private _phoneNumber: string='0000000000';*/
+
+  /* constructor(firstname:string,lastname:string){
+     this._firstname=firstname;
+     this._lastname=lastname;
+   }
+
+  public  build (): User{
+  /!* assert firstname!=null;
+   assert lastname!=null;
+   assert email!=null;
+   assert pwhash!=null;
+ *!/
+   return new User(this);//firstname:string, lastname: string, email: string, pwhash: string, isFirm: boolean);
+   }
+
+
+ */
+
+  constructor() {
+    this._firstname= '';
+this._lastname= '';
+  this._email= '';
+ this._pwhash= '';
+  this._address=mockAddress;
+  this._isVerified=false;
+ this._isFirm=false;
+ // this._phoneNumber='0000000000';
+  }
+  public static user(): UserBuilder{
+    return new UserBuilder();
+  }
+
+  public setFirstname(firstname: string):UserBuilder{
     this._firstname=firstname;
-    this._lastname=lastname;
-  } ;
-
- // public  build (): User{
- /* assert firstname!=null;
-  assert lastname!=null;
-  assert email!=null;
-  assert pwhash!=null;
-*/
-//  let user: User= new User(this);//firstname:string, lastname: string, email: string, pwhash: string, isFirm: boolean);
-//  return user;
-
-
-
-  public setEmail (email:string){
-    this._email= email;
     return this;
+  }
+
+  public setLastname(lastname: string):UserBuilder{
+    this._lastname=lastname;
+    return this;
+  }
+  public setEmail(email: string):UserBuilder{
+    this._email=email;
+    return this;
+  }
+  public setPwhash(pwhash: string):UserBuilder{
+    this._pwhash=pwhash;
+    return this;
+  }
+  public setAddress(address: Address):UserBuilder{
+    this._address=address;
+    return this;
+  }
+
+  public setIsVerified(isVerified:boolean):UserBuilder{
+    this._isVerified=isVerified;
+    return this;
+  }
+
+  public setIsFirm(isFirm: boolean):UserBuilder{
+    this._isFirm=isFirm;
+    return this;
+  }
+public build (): User{
+    let user: User= new User(this._firstname, this._lastname, this._email, this._pwhash, this._isVerified, this._address, this._isFirm);
+    return user;
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  public setEmail (email:string){
+    this.email= email;
+    return this;
+  }
+
+  public getEmail(): string {
+   return this.email;
+
   }
   public setPwhash (pwhash:string){
-    this._pwhash= pwhash;
+    this.pwhash= pwhash;
+    return this;
+  }
+  public getPwHash(): string {
+    return this.pwhash;
+
+  }
+
+  public setIsFirm (isFirm:boolean){
+    this.isFrim= isFirm;
     return this;
   }
 
-  public setisFirm (isFirm:boolean){
-    this._isFrim= isFirm;
-    return this;
+  public getIsFirm(): boolean {
+    return this.isFrim;
+
   }
   public setAddress (address:Address){
-    this._address= address;
+    this.address= address;
     return this;
+  }
+  public getAddress(): Address {
+    return this.address;
+
+  }
+  public build(){
+  return new User(this);
   }
 }
 
