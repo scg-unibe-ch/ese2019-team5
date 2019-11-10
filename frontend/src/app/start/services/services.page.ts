4ae2639bbd70eab3ab1774ea69c2f1ea060a401b
Vishva R <vishva_15@hotmail.com>
2019-11-10 22:01:10 +0100

    Created Page for listing all services

--- frontend/src/app/start/services/services.page.ts
+++ frontend/src/app/start/services/services.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService} from "../../../../../backend/app/models/eventService.model";
+import {EventServiceContainer} from "../../../../../backend/app/models/eventServiceContainer.model";
+import {userJson} from "../userprofile/userJson";
+import {HttpClient} from "@angular/common/http";
+import {forEach} from "@angular-devkit/schematics";
+import {map, tap} from "rxjs/operators";
+import {eventJson} from "./eventJson";
 
 @Component({
   selector: 'app-services',
@@ -6,12 +13,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.page.scss'],
 })
 export class ServicesPage implements OnInit {
+  eventList: any[] = [];
 
-  readonly ROOT_URL = 'http://localhost:3000/services/';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    ) { }
 
-  ngOnInit() {
+  getServicesFromBackend(){
+  this.http.get<Array<any>>('http://localhost:3000/search')
+    .subscribe(
+      (data)=> {
+        this.eventList = data;
+      }, (error) => {
+        console.log(error);
+      });
   }
 
+  private update(){
+    this.getServicesFromBackend()
+  }
+
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+    this.update();
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 1200);
+  }
+
+  getIconName(category:string){
+    switch (category){
+      case 'Music': {
+        return 'musical-notes';
+      }
+      case 'Location': {
+        return 'compass';
+      }
+      case 'Food': {
+        return 'pizza';
+      }
+      case 'Gastronomy': {
+        return 'pizza';
+      }
+      case 'Entertainment': {
+        return 'game-controller-b';
+      }
+      case 'Photography': {
+        return 'camera';
+      }
+      default: {
+        return 'infinite';
+      }
+    }
+  }
+
+
+
+  ngOnInit() {
+    this.getServicesFromBackend();
+  }
 }
