2681b1ad4d2a680f0f78e6365660a0a3ba1d4ae7
Gillian Cathomas <gillian.cathomas@gmx.ch>
2019-11-13 12:16:17 -0700

    ignore last commit, small search http restpoint for category, city and price

--- backend/app/controllers/search.controller.ts
+++ backend/app/controllers/search.controller.ts
@@ -9,6 +9,8 @@ import * as fs from 'fs';
 import {UserBuilder} from "../models/userBuilder.model";
 import {EventServiceContainer} from "../models/eventServiceContainer.model";
 import {EventService} from "../models/eventService.model";
+import {EventServiceFilter} from "../models/eventServiceFilter.model";
+import {FilterCategories} from "../models/filterCategories.enum";
 
 const router: Router = Router(); // part of express needed
 
@@ -17,21 +19,19 @@ const router: Router = Router(); // part of express needed
 const dbService = new DbServices();
 
 
-
-
 router.get('/', async (req: Request, res: Response) => {
 
 
     try {
-      let allServicesContainer:EventServiceContainer=await dbService.getAllServices();
-      let EventServicesArray: EventService []=allServicesContainer.getServices();
-    res.send(EventServicesArray.map(e=>e.toSimplification()));
-     // let allEventService={
+      let allServicesContainer: EventServiceContainer = await dbService.getAllServices();
+      let EventServicesArray: EventService [] = allServicesContainer.getServices();
+      res.send(EventServicesArray.map(e => e.toSimplification()));
+      // let allEventService={
       //  'allServicesContainer': allServicesContainer,
-     // }
+      // }
       res.status(200);
       //res.send(allEventService);
-
+ console.log('not really where you want to be');
     } catch (error) {
       res.status(404);
       res.send('error in backend' + error.message);
@@ -42,17 +42,25 @@ router.get('/', async (req: Request, res: Response) => {
 );
 
 
-router .get('/:filter',async (req: Request, res: Response) => {
-
-
+router.get('/filter/:category/:city?/:price?', async (req: Request, res: Response) => {
   try {
+    let category: string = req.query.category;
+    let city: string = req.query.city
+    console.log('got here');
+    console.log(city);
 
+    console.log(category);
+    let serviceCFittingRequest: EventServiceContainer = await dbService.getServiceFilter([new EventServiceFilter(FilterCategories.category, category)]);
+    let serviceAOfFittingRequest: EventService[] = serviceCFittingRequest.getServices();
+    res.status(200).send(serviceAOfFittingRequest.map(e => e.toSimplification()))
+
+    console.log(serviceAOfFittingRequest);
   } catch (error) {
-      res.status(404);
-      res.send('error in backend' + error.message);
-      console.log(error);
-    }
-  })
+    res.status(404);
+    res.send('error in backend' + error.message);
+    console.log(error);
+  }
+})
 
 
 export const SearchController: Router = router;
