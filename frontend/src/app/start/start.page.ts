d8e9d7c23b936812964f8053269a9c9bfb63d94f
Sophie Pfister <sophie.pfister@students.unibe.ch>
2019-11-19 15:59:09 +0100

    Moved content of "EventServicePage" to StartPage but did not delete EventServicePage yet as it is still used in routing. Also created Form to set search-parameters.
    Next, searching will be implemented

--- frontend/src/app/start/start.page.ts
+++ frontend/src/app/start/start.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../AuthService/auth.service";
+import {EventService} from "../../../../backend/app/models/eventService.model";
+import {SearchService} from "../SearchService/search.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
-//ToDo: Profil-Seite (Lars) mit Link zu Service erstellen
 
-//ToDo Visu: div. AngebotPage
-// Zeigt alle in der DB gespeicherten Angebote an
 
 @Component({
   selector: 'app-start',
@@ -12,15 +13,58 @@ import {AuthService} from "../AuthService/auth.service";
   styleUrls: ['./start.page.scss'],
 })
 export class StartPage implements OnInit {
-  isLoggedIn(){
-    return this.authService.isLoggedIn()
+
+  loading: boolean;
+  services: EventService[];
+
+
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient,
+    private searchService: SearchService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  searchForm = this.formBuilder.group({
+      category: [''],
+      subtype: [''],
+      city: [''],
+      price: [''],
+      persons: [''],
+      text: ['']
+    });
+
+  get category () {
+    return this.searchForm.get('category');
+  }
+  get subtype() {
+    return this.searchForm.get('subtype');
+  }
+  get city() {
+    return this.searchForm.get('city');
+  }
+  get price() {
+    return this.searchForm.get('price');
+  }
+  get text() {
+    return this.searchForm.get('text');
   }
 
-  constructor(private authService: AuthService) {
+  search() {
 
   }
 
-  ngOnInit() {
+ ngOnInit() {
+    this.loading = true;
+    this.http.get<Array<EventService>>('http://localhost:3000/search')
+      .subscribe(
+        (data)=> {
+          this.services = data;
+        }, (error) => {
+          console.log(error);
+        });
+    this.loading = false;
+
   }
 
 }
