46910c39c35d5426627936a49f9e1fbeb3c3ff65
Lars Ziegler <lars.ziegler@students.unibe.ch>
2019-12-02 22:09:04 +0100

    Quickfix for display of availability, that was causing detail page not to load properly

--- frontend/src/app/start/EventServices/event-service-detail/event-service-detail.page.ts
+++ frontend/src/app/start/EventServices/event-service-detail/event-service-detail.page.ts
@@ -174,13 +174,6 @@ export class EventServiceDetailPage implements OnInit {
     return priceFormat;
   };
 
-  getEveryAvailableDay(): string{
-    let x = '';
-    for(let day of this.availability){
-      x += day;
-    }
-    return x;
-  }
 
   redirectStartPage() {
     document.location.href = 'http://localhost:4200/start/';
