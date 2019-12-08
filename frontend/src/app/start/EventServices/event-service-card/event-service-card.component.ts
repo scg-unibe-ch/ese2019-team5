4da05d390d47a58dbe75fa8b79e057f7510cb0c0
Lars Ziegler <lars.ziegler@students.unibe.ch>
2019-12-08 18:39:11 +0100

    Made width of event cards flexible

--- frontend/src/app/start/EventServices/event-service-card/event-service-card.component.ts
+++ frontend/src/app/start/EventServices/event-service-card/event-service-card.component.ts
@@ -14,6 +14,7 @@ import {Observable, Subject} from "rxjs";
 
 export class EventServiceCardComponent implements OnInit {
   @Input() services: EventService[];
+  @Input() width: any;
 
   constructor(
     private auth: AuthService,
