5f026670f9c50b10e21d8648ae61db62ed18b497
Sophie Pfister <sophie.pfister@students.unibe.ch>
2019-12-10 12:39:41 +0100

    Added and improved datetimepickers in EventServicePage

--- frontend/src/app/start/EventServices/event-service-detail/event-service-detail.page.ts
+++ frontend/src/app/start/EventServices/event-service-detail/event-service-detail.page.ts
@@ -7,6 +7,7 @@ import {ToastController} from "@ionic/angular";
 import {AuthService} from "../../../AuthService/auth.service";
 import {EventServiceJson} from "../../userprofile/EventServiceJson";
 import {format} from 'date-fns';
+import * as moment from 'moment';
 
 import {Observable, Subject, Subscription} from "rxjs";
 import {Platform} from "@ionic/angular";
@@ -17,6 +18,8 @@ import {Platform} from "@ionic/angular";
   styleUrls: ['./event-service-detail.page.scss'],
 })
 export class EventServiceDetailPage implements OnInit {
+  private today;
+  private year;
   public devWidth = this.platform.width();
 
   private serviceId: string;
@@ -117,6 +120,7 @@ export class EventServiceDetailPage implements OnInit {
 
 
   ngOnInit() {
+
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if(!paramMap.has('serviceId')){
         //redirect
@@ -130,6 +134,8 @@ export class EventServiceDetailPage implements OnInit {
 
 
   showOfferInput() {
+    this.today = moment().format("YYYY-MM-DD").toString();
+    this.year = moment().add(1, "year").format("YYYY-MM-DD").toString();
     this.isInputing = true;
   }
 
