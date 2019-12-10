84e8eb373542dd258401c40eebc5818193a1a9e4
Gillian Cathomas <gillian.cathomas@gmx.ch>
2019-12-10 12:37:00 -0800

    minor changes

--- backend/app/services/emailServices/emailForSignUpCreator.service.ts
+++ backend/app/services/emailServices/emailForSignUpCreator.service.ts
@@ -10,9 +10,9 @@ export class EmailForSignUpCreatorService{
       "<div align=center style='font-size:24.0pt;font-family:\"Arial Black\",sans-serif'>Welcome to Eventdoo<\div>" +
     "<div align=center style='font-size:14.0pt;font-family:\"Arial\",sans-serif'>"+
       "<p>We are pleased to welcome you as a new Eventdoo user.</p>"+
-    "<p>In Order to use all our services, we kindly ask you to verify your email-address.</p>"+
+    "<p>In order to use all our services, we kindly ask you to verify your email-address.</p>"+
     "<a href='" +token +"' target='_blank'>verify email</a>"+
-      "<p>Have fun planning your event or finding new oppurtunities to offer your services</p>"+
+      "<p>Have fun planning your event or finding new oppurtunities to offer your services.</p>"+
     "<p><b><i>Your Eventdoo Team</i></b></p>"+
     "</div>"+
     "<div align=center style='font-size:10.0pt;font-family:\"Arial\",sans-serif'>"+
