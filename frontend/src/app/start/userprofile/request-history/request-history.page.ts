ad4cf7204b3d3f900985f58c9b31560d3f462d10
Sophie Pfister <sophie.pfister@students.unibe.ch>
2019-12-10 21:27:25 +0100

    Added/updated comments in and improved RequestHistoryPage

--- frontend/src/app/start/userprofile/request-history/request-history.page.ts
+++ frontend/src/app/start/userprofile/request-history/request-history.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../../AuthService/auth.service";
 import {HttpClient} from "@angular/common/http";
 import {ServiceRequest} from "../../../../../../backend/app/models/serviceRequest.model";
+import {error} from "util";
+
+/**
+ * Displays all services a user has requested so far
+ * (Available only for authenticated users)
+ */
 
 @Component({
   selector: 'app-request-history',
@@ -12,21 +18,37 @@ export class RequestHistoryPage implements OnInit {
   userId: string;
   requests: ServiceRequest[];
   empty: boolean;
+  error: string;
 
   constructor(private authService: AuthService,
-              private http: HttpClient) {}
-
-  ngOnInit() {
-    this.empty = false;
-    this.userId = this.authService.getUserId();
-    this.http.get('http://localhost:3000/profile/requestedServices/' + this.userId).subscribe(
-      (data) => { this.requests = <ServiceRequest[]>data;
-      if (this.requests.length == 0)
-        this.empty = true;},
-    (error) => {console.log(error)}
-    );
+              private http: HttpClient) {
   }
 
+  /**
+   * Gets all services the user has requested so far from backend
+   */
+  ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.empty = false;
+      this.userId = this.authService.getUserId();
+      this.http.get('http://localhost:3000/profile/requestedServices/' + this.userId).subscribe(
+        (data) => {
+          this.requests = <ServiceRequest[]>data;
+          if (this.requests.length == 0)
+            this.empty = true;
+        },
+        (error) => {
+          console.log(error)
+        }
+      );
+    } else
+      this.error = 'You are not authenticated. Please log in or sign up.'
+  }
+
+  /**
+   * Returns the icon's name according to a service's category
+   * @param category
+   */
   getIcon(category: string) {
     switch (category) {
       case 'Music': {
@@ -49,6 +71,7 @@ export class RequestHistoryPage implements OnInit {
       }
     }
   }
+
   navigateToDetailPage(serviceId: string) {
     document.location.href = 'http://localhost:4200/start/services/'+serviceId;
   }
